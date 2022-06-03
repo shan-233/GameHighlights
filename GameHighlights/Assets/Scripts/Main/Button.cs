5be412bab514d6b8321fa7597c9a3d75a7ab51d3
Shan <Shan3program@gmail.com>
2022-06-03 17:03:51 +0800

    新增－Main頁點擊按鈕跳到記憶翻牌頁面

---
+++ GameHighlights/Assets/Scripts/Main/Button.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void goToMemoryFlop(){ //點擊翻牌配對的按鈕觸發這個function
+        SceneManager.LoadScene(1); // 到MemoryFlop Scenes
+    }
+}
