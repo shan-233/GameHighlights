cb845220696fa1ec1d2fa4910dcf65984648b76f
Shan <Shan3program@gmail.com>
2022-06-03 17:21:24 +0800

    新增－記憶翻牌Game、Result、Main場景

--- GameHighlights/Assets/Scripts/Main/Button.cs
+++ GameHighlights/Assets/Scripts/Main/Button.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+// 主頁的按鈕
 public class Button : MonoBehaviour
 {
     void Start()
@@ -16,7 +17,7 @@ public class Button : MonoBehaviour
         
     }
 
-    public void goToMemoryFlop(){ //點擊翻牌配對的按鈕觸發這個function
+    public void goToMemoryFlop(){ // 點擊翻牌配對的按鈕觸發這個function
         SceneManager.LoadScene(1); // 到MemoryFlop Scenes
     }
 }
