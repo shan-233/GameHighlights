00c2ff7af3de501e4132cf677f536c5668caf7b2
Shan <Shan3program@gmail.com>
2022-06-05 00:16:27 +0800

    新增－遊戲場景&程式

---
+++ GameHighlights/Assets/Scripts/MemoryFlop/MF_Game.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class MF_Game : MonoBehaviour
+{
+    public GameObject back1;
+    public GameObject front1;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void touchBack1(){
+        if (back1.activeInHierarchy) // 判斷back1是否為顯示狀態
+        {
+            back1.SetActive(false);
+        }else
+        {
+            back1.SetActive(false);
+        }
+        
+    }
+}
